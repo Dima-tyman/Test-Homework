bd6eb694ac421605a85ecf79c6d64e021b8976e4
Dima-tyman <110516177+Dima-tyman@users.noreply.github.com>
2022-09-05 07:39:06 +0300

    Create task34.cs

---
+++ task34.cs
@@ -0,0 +1,32 @@
+Console.Clear();
+        int lengthArray;
+        Console.Write("Введите длину массива: ");
+        while (!int.TryParse(Console.ReadLine(), out lengthArray)) {
+        Console.Write("Ошибка! Введите число: ");
+        }
+
+        int[] array = new int[lengthArray > 0 ? lengthArray : -lengthArray];
+
+        void FillArray(int[] arr) {
+            for (int i = 0; i < arr.Length; i++) {
+                arr[i] = new Random().Next(100, 1000);
+            }
+        }
+
+        void WriteArray(int[] arr) {
+            Console.Write($"Массив: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+        
+        FillArray(array);
+        int count = 0;
+        for (int i = 0; i < array.Length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+        WriteArray(array);
+        Console.WriteLine(" ");
+        Console.WriteLine(count);
