ff2c291403833d7d02f5d6638422e05f4809502e
Dima-tyman <110516177+Dima-tyman@users.noreply.github.com>
2022-09-05 07:49:59 +0300

    Create task36.cs

---
+++ task36.cs
@@ -0,0 +1,30 @@
+        Console.Clear();
+        int lengthArray;
+        Console.Write("Введите длину массива: ");
+        while (!int.TryParse(Console.ReadLine(), out lengthArray)) {
+        Console.Write("Ошибка! Введите число: ");
+        }
+
+        int[] array = new int[lengthArray > 0 ? lengthArray : -lengthArray];
+
+        void FillArray(int[] arr) {
+            for (int i = 0; i < arr.Length; i++) {
+                arr[i] = new Random().Next(-99, 100);
+            }
+        }
+
+        void WriteArray(int[] arr) {
+            Console.Write($"Массив: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+        
+        FillArray(array);
+        int sum = 0;
+        for (int i = 1; i < array.Length; i += 2) {
+            sum += array[i];
+        }
+        WriteArray(array);
+        Console.WriteLine();
+        Console.WriteLine(sum);
